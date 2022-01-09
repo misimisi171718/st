210d3d877c3979417eabacb6ed7ac6b3dd4118c9
misimisi171718 <sumegimisi02@gmail.com>
2022-01-09 11:37:59 +0100

    alpha

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -124,7 +124,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#181818", /* default background colour */
 };
 
 
